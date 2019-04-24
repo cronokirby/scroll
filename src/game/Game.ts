912214463b982682e63602710131f1b9b80e7fda
Lúcás Meier <cronokirby@gmail.com>
2019-04-24 20:22:56 +0200

    Document game

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -2,10 +2,22 @@ import * as PIXI from 'pixi.js';
 import { Control, Controller } from '../controller';
 import { SpriteSheet } from '../sprites';
 
-
+/**
+ * Represents an entire Game.
+ * 
+ * The game is responsible for reacting to user input,
+ * advancing the state of the game, and setting up the
+ * sprites in the stage for rendering.
+ */
 class Game {
     private _player: PIXI.Sprite;
 
+    /**
+     * Construct a new game given sprite and control information.
+     * 
+     * @param _sheet the sprite sheet for this game
+     * @param _controller the controller for player input
+     */
     constructor(private _sheet: SpriteSheet, private _controller: Controller) {
         this._player = this._sheet.indexSprite(0, 0);
         this._controller.onPress(Control.Left, () => {
@@ -22,6 +34,14 @@ class Game {
         });
     }
 
+    /**
+     * Set the stage for this game, ready for drawing.
+     * 
+     * This should be called after the game is instantiated in order
+     * to set up rendering.
+     * 
+     * @param container the container to draw things inside
+     */
     setStage(container: PIXI.Container) {
         container.addChild(this._player);
     }
