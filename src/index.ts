207872e0c608fc6d55406cb60cda4e05acc9d511
Lúcás Meier <cronokirby@gmail.com>
2020-11-02 19:14:02 +0100

    Remove sidebar (for now)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as PIXI from "pixi.js";
 import { Controller } from "./controller";
-import { GRID_SIZE, SIDE_PANEL_SIZE, SPRITE_SIZE } from "./dimensions";
+import { GRID_SIZE, SPRITE_SIZE } from "./dimensions";
 import { SpriteSheet } from "./sprites";
 
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 const app = new PIXI.Application({
-  width: SIDE_PANEL_SIZE + SPRITE_SIZE * GRID_SIZE,
+  width: SPRITE_SIZE * GRID_SIZE,
   height: SPRITE_SIZE * GRID_SIZE,
   resolution: devicePixelRatio,
 });
