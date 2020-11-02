e50b02a30ccf9e035050a6ecb41bb2bd1c7f8d9d
Lúcás Meier <cronokirby@gmail.com>
2020-11-02 19:13:14 +0100

    Remove unnecessary stuff to start from scratch

--- src/game/components/Consume.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This class is used to contain the results of consuming an item.
- */
-interface Consume {
-    restoreHealth: number;
-}
-export default Consume;
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,27 @@
-import * as PIXI from 'pixi.js';
-import { Controller } from './controller';
-import { SpriteSheet } from './sprites';
-import { GRID_SIZE, SIDE_PANEL_SIZE, SPRITE_SIZE } from './dimensions';
-import Game from './game/Game';
-
+import * as PIXI from "pixi.js";
+import { Controller } from "./controller";
+import { GRID_SIZE, SIDE_PANEL_SIZE, SPRITE_SIZE } from "./dimensions";
+import { SpriteSheet } from "./sprites";
 
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 const app = new PIXI.Application({
-    width: SIDE_PANEL_SIZE + SPRITE_SIZE * GRID_SIZE,
-    height: SPRITE_SIZE * GRID_SIZE,
-    resolution: devicePixelRatio
+  width: SIDE_PANEL_SIZE + SPRITE_SIZE * GRID_SIZE,
+  height: SPRITE_SIZE * GRID_SIZE,
+  resolution: devicePixelRatio,
 });
 
-window.addEventListener('load', () => {
-    const gameEl = document.getElementById('game');
-    if (gameEl) {
-        gameEl.appendChild(app.view);
-    }
-})
-
+window.addEventListener("load", () => {
+  const gameEl = document.getElementById("game");
+  if (gameEl) {
+    gameEl.appendChild(app.view);
+  }
+});
 
 const controller = new Controller();
-PIXI.loader
-    .add(SpriteSheet.deps)
-    .load(() => {
-        const game = new Game(controller);
-        game.setStage(app.stage);
-    });
+
+PIXI.loader.add(SpriteSheet.deps).load(() => {
+  const sheet = new SpriteSheet();
+  const sprite = sheet.indexSprite(0, 0);
+  app.stage.addChild(sprite);
+});
